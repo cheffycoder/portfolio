c7506600eec24bfbad6a850f0323953eb2625d66
shivam.b <shivam.b@loginextsolutions.com>
2023-06-05 19:51:17 +0530

    Skills Filter completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
+// Filters Tabs
+const tabs = document.querySelectorAll('[data-target');
+const tabContents = document.querySelectorAll('[data-content]');
 
+const onTabClick = 
+
+// Adding Click event listener to both tabs.
+tabs.forEach(tab => tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tc) => {
+        // tc = tabContents
+        tc.classList.remove('filters__active');
+    })
+    target.classList.add('filters__active');
+
+    tabs.forEach(t => { // t = tab
+        t.classList.remove('filter-tab-active')
+    })
+    tab.classList.add('filter-tab-active');
+}))
